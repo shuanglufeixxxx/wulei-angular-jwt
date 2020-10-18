f4a93ae735847d590bb236c01925868fcdedd507
Lufei Shuang <shuanglufei1994@gmail.com>
2020-10-18 09:33:11 +0700

    skip X-XSRF-TOKEN for idempotent request method

--- src/app/shared/restangularConfig.ts
+++ src/app/shared/restangularConfig.ts
@@ -2,16 +2,21 @@ import { CookieService } from "ngx-cookie-service";
 import { baseURL } from "./baseURL";
 import { InjectorWrapper } from "./InjectorWrapper";
 
+const idempotentRequestMethods = ['get', 'head', 'options', 'trace'];
+
 export function restangularConfig(RestangularProvider) {
   RestangularProvider
     .setBaseUrl(baseURL)
     .setPlainByDefault(true)
     .addFullRequestInterceptor(
-      (element, operation, path, url, headers, params) => {
+      (element, operation: string, path, url, headers, params) => {
+        if ( idempotentRequestMethods.some(method => operation.includes(method)) ) {
+          return {}
+        }
         const cookieService = InjectorWrapper.injector.get(CookieService);
         const token = cookieService.get("XSRF-TOKEN");
         return {
-          headers: Object.assign({}, headers, { "X-XSRF-TOKEN": token }),
+          headers: Object.assign({}, headers, { "X-XSRF-TOKEN": token })
         };
       }
     );
